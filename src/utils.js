b2fd8701383298ed0728311a13a229c3eb9b3ec9
Steven Loria <sloria1@gmail.com>
2018-07-26 22:07:10 -0400

    Remove strange characters when slugifying

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,8 @@
 import slugify from "slugify";
 
 export const slugifyEntry = entry =>
-  `${formatDate(entry.date)}-${slugify(entry.title, { lower: true })}`;
+  `${formatDate(entry.date)}-${slugify(entry.title, {
+    lower: true,
+    remove: /[$*_+~.()'"!\-:@]/g
+  })}`;
 export const formatDate = date => date.toISOString().slice(0, 10); // 2020-02-23
--- src/utils.test.js
+++ src/utils.test.js
@@ -9,4 +9,13 @@ describe("slugifyEntry", () => {
     const result = slugifyEntry(entry);
     expect(result).toBe("2020-01-23-foo-bar-baz");
   });
+
+  it("should remove quotes", () => {
+    const entry = {
+      title: "'Foo' \"Bar\" Baz",
+      date: new Date(2020, 0, 23)
+    };
+    const result = slugifyEntry(entry);
+    expect(result).toBe("2020-01-23-foo-bar-baz");
+  });
 });
