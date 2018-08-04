3be39f9a3d31b163e4a7eec1856ad6d6f5112db8
Steven Loria <sloria1@gmail.com>
2018-08-04 08:18:00 -0400

    Remove "=" from slugs

--- src/utils.js
+++ src/utils.js
@@ -3,7 +3,7 @@ import slugify from "slugify";
 export const slugifyEntry = entry =>
   `${formatDate(entry.date)}-${slugify(entry.title, {
     lower: true,
-    remove: /[$*_+~.()'"!\-:@]/g
+    remove: /[$*_+~.()'"!\-:@=]/g
   })}`;
 export const formatDate = date => date.toISOString().slice(0, 10); // 2020-02-23
 
